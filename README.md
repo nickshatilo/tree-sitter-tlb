8e60d1474f9014f8860b97e9d5cd185fd76db9ce
Nick Shatilo <shatilo.nick@gmail.com>
2024-12-07 17:24:00 +0000

    added readme

---
+++ README.md
@@ -0,0 +1,50 @@
+# tree-sitter-tlb
+
+A Tree-sitter grammar for the [TL-B](https://docs.ton.org/v3/documentation/data-formats/tlb/tl-b-language) language.
+
+Includes nvim bindings.
+
+
+## Installation
+
+Lazy.nvim:
+
+```lua
+    {
+        'nickshatilo/tree-sitter-tlb',
+        lazy = false,
+        opts = {},
+    }
+```
+
+## Grammar references
+
+* [Official docs](https://docs.ton.org/v3/documentation/data-formats/tlb/tl-b-language)
+* [TL-B parser](https://github.com/ton-community/tlb-parser)
+* [Native TL-B types](https://github.com/ton-blockchain/ton/blob/master/crypto/block/block.tlb)
+
+## Func comments injections
+
+I also like to have TL-B schemes highlighted right inside func files, so if you want you could add the following to your ~/.config/nvim/queries/func/injections.scm file:
+
+```
+(
+  (comment) @injection.content
+  (#match? @injection.content "\\{-TLB(.*)-\\}")
+  (#offset! @injection.content 0 5 0 -2)
+  (#set! injection.language "tlb")
+)
+```
+
+So inside func files you could have highlighting inside the comments:
+
+```func
+{-TLB
+    addr_none$00 = MsgAddressExt;
+-}
+```
+
+## TODO:
+
+* Add TL-B into official [nvim-treesitter](https://github.com/nvim-treesitter/nvim-treesitter) grammars
+
--- grammar.js
+++ grammar.js
@@ -1,12 +1,9 @@
 module.exports = grammar({
-  name: 'tlb',
+  name: "tlb",
 
-  extras: $ => [
-    $.comment,
-    /\s/
-  ],
+  extras: ($) => [$.comment, /\s/],
 
-  conflicts: $ => [
+  conflicts: ($) => [
     [$._ref_expression, $.type_application],
     [$.field_anonymous_def],
     [$._type_expression, $._type_expression_no_application],
@@ -18,266 +15,240 @@ module.exports = grammar({
     [$._field_value, $._expression],
   ],
 
-  precedences: _ => [
-    ['sequence'],
-    ['declaration'],
-    ['combinator_args'],
-    ['comparison'],
-    ['type_math'],
-    ['math'],
-    ['conditional'],
-    ['application'],
-    ['cell_ref'],
-    ['prefix', 'unary'],
-    ['primary'],
-    ['atom'],
+  precedences: (_) => [
+    ["sequence"],
+    ["declaration"],
+    ["combinator_args"],
+    ["comparison"],
+    ["type_math"],
+    ["math"],
+    ["conditional"],
+    ["application"],
+    ["cell_ref"],
+    ["prefix", "unary"],
+    ["primary"],
+    ["atom"],
   ],
 
   rules: {
-    program: $ => repeat($._statement),
+    program: ($) => repeat($._statement),
 
-    _statement: $ => choice(
-      $.declaration,
-      $.comment
-    ),
+    _statement: ($) => choice($.declaration, $.comment),
 
-    declaration: $ => prec.right('declaration', seq(
-      optional('!'),
-      $.constructor,
-      repeat($.field_definition),
-      '=',
-      $.combinator,
-      ';'
-    )),
-
-    constructor: $ => prec.left(seq(
-      $.constructor_name,
-      optional($.constructor_tag)
-    )),
-
-    constructor_name: $ => choice(
-      '_',
-      $.identifier
-    ),
-
-    constructor_tag: $ => choice(
-      seq('$', choice('_', $._binary_number)),
-      seq('#', choice('_', $._hex_number))
-    ),
-
-    field_definition: $ => prec.right('sequence', choice(
-      $.field_parameter_def,
-      $.field_curly_expr_def,
-      $.field_named_def,
-      $.field_anonymous_def,
-      $.field_expr_def
-    )),
-
-    field_parameter_def: $ => seq(
-      '{',
-      repeat1(seq(
-        $.identifier,
-        ':',
-        $.type_identifier
-      )),
-      '}'
-    ),
-
-    field_curly_expr_def: $ => seq(
-      '{',
-      $._expression,
-      '}'
-    ),
-
-    field_named_def: $ => seq(
-      $.identifier,
-      ':',
-      $._field_value
-    ),
-
-    _field_value: $ => choice(
-      $.cell_ref_expression,
-      $.conditional_expression,
-      $._type_expression
-    ),
-
-    field_anonymous_def: $ => prec.right('sequence', seq(
-      optional('^'),
-      '[',
-      repeat($.field_definition),
-      ']'
-    )),
-
-    field_expr_def: $ => $._type_expression,
-
-    combinator: $ => seq(
-      $.identifier,
-      prec.right('combinator_args', repeat($._type_expression))
-    ),
-
-    _expression: $ => choice(
-      prec('comparison', $.comparison_expression),
-      $.math_expression,
-      $.conditional_expression,
-      $.bit_selection_expression,
-      prec('unary', $.negate_expression),
-      $.parenthesized_expression,
-      prec('primary', $._ref_expression)
-    ),
-
-    conditional_expression: $ => prec.right('conditional', seq(
-      $._expression,
-      '?',
-      $._type_expression
-    )),
-
-    _type_expression: $ => choice(
-      prec('application', $.type_application),
-      $.cell_ref_expression,
-      $.builtin_expression,
-      $.type_math_expression,
-      prec('unary', $.negate_expression),
-      $.parenthesized_type_expression,
-      prec('primary', $._ref_expression),
-      '#'
-    ),
-
-    _type_expression_no_application: $ => choice(
-      $.builtin_expression,
-      $.type_math_expression,
-      prec('unary', $.negate_expression),
-      $.parenthesized_type_expression,
-      prec('primary', $._ref_expression),
-      '#'
-    ),
-
-    _type_app_argument: $ => choice(
-      $._simple_type_expr,
-      prec.right(1, $.cell_ref_expression),
-      prec.right(1, $.negate_expression),
-      prec.right(1, $.builtin_expression),
-      $._number
-    ),
-
-    _simple_type_expr: $ => choice(
-      $.identifier,
-      'Any',
-      $._number,
-      $.parenthesized_type_expression
-    ),
-
-    math_expression: $ => prec.left('math', choice(
-      seq($._expression, '+', $._expression),
-      seq($._expression, '-', $._expression),
-      seq($._expression, '*', $._expression),
-      seq($._expression, '/', $._expression),
-    )),
-
-    type_math_expression: $ => prec.left('type_math', choice(
-      seq($._type_expression, '+', $._type_expression),
-      seq($._type_expression, '-', $._type_expression),
-      seq($._type_expression, '*', $._type_expression),
-      seq($._type_expression, '/', $._type_expression)
-    )),
-
-    comparison_expression: $ => prec.left('comparison', seq(
-      $._expression,
-      choice('<=', '>=', '!=', '=', '<', '>'),
-      $._expression
-    )),
-
-    bit_selection_expression: $ => seq(
-      $._expression,
-      '.',
-      $._number
-    ),
-
-    negate_expression: $ => prec.right('unary', seq(
-      '~',
-      choice(
-        $._ref_expression,
-        $.parenthesized_expression,
-        $.parenthesized_type_expression
-      )
-    )),
-
-    cell_ref_expression: $ => prec.right('cell_ref', seq(
-      '^',
-      choice(
-        $.identifier,
-        $.field_anonymous_def,
-        $.parenthesized_type_application
-      )
-    )),
-
-    type_application: $ => prec.right('application', seq(
-      $.identifier,
-      repeat1($._type_app_argument)
-    )),
-
-    parenthesized_type_application: $ => seq(
-      '(',
-      $.identifier,
-      repeat1($._type_expression),
-      ')'
-    ),
-
-    builtin_expression: $ => seq(
-      '(',
-      choice(
-        seq($.builtin_operator, choice($._expression, $._number)),
-        $.builtins_zero_args
+    declaration: ($) =>
+      prec.right(
+        "declaration",
+        seq(
+          optional("!"),
+          $.constructor,
+          repeat($.field_definition),
+          "=",
+          $.combinator,
+          ";",
+        ),
       ),
-      ')'
-    ),
 
-    builtin_operator: $ => choice(
-      '##',
-      '#<',
-      '#<=',
-      'lenBits'
-    ),
+    constructor: ($) =>
+      prec.left(seq($.constructor_name, optional($.constructor_tag))),
 
-    builtins_zero_args: $ => '#',
+    constructor_name: ($) => choice("_", $.identifier),
 
-    _ref_expression: $ => prec('atom', choice(
-      $.identifier,
-      $._number
-    )),
+    constructor_tag: ($) =>
+      choice(
+        seq("$", choice("_", $._binary_number)),
+        seq("#", choice("_", $._hex_number)),
+      ),
 
-    parenthesized_expression: $ => seq(
-      '(',
-      $._expression,
-      ')'
-    ),
+    field_definition: ($) =>
+      prec.right(
+        "sequence",
+        choice(
+          $.field_parameter_def,
+          $.field_curly_expr_def,
+          $.field_named_def,
+          $.field_anonymous_def,
+          $.field_expr_def,
+        ),
+      ),
 
-    parenthesized_type_expression: $ => seq(
-      '(',
-      $._type_expression,
-      ')'
-    ),
+    field_parameter_def: ($) =>
+      seq("{", repeat1(seq($.identifier, ":", $.type_identifier)), "}"),
 
-    comment: $ => token(choice(
-      seq('//', /.*/),
+    field_curly_expr_def: ($) => seq("{", $._expression, "}"),
+
+    field_named_def: ($) => seq($.identifier, ":", $._field_value),
+
+    _field_value: ($) =>
+      choice(
+        $.cell_ref_expression,
+        $.conditional_expression,
+        $._type_expression,
+      ),
+
+    field_anonymous_def: ($) =>
+      prec.right(
+        "sequence",
+        seq(optional("^"), "[", repeat($.field_definition), "]"),
+      ),
+
+    field_expr_def: ($) => $._type_expression,
+
+    combinator: ($) =>
       seq(
-        '/*',
-        /[^*]*\*+([^/*][^*]*\*+)*/,
-        '/'
-      )
-    )),
+        $.identifier,
+        prec.right("combinator_args", repeat($._type_expression)),
+      ),
 
-    identifier: $ => /[a-zA-Z_][a-zA-Z0-9_]*/,
+    _expression: ($) =>
+      choice(
+        prec("comparison", $.comparison_expression),
+        $.math_expression,
+        $.conditional_expression,
+        $.bit_selection_expression,
+        prec("unary", $.negate_expression),
+        $.parenthesized_expression,
+        prec("primary", $._ref_expression),
+      ),
 
-    type_identifier: $ => choice(
-      'Type',
-      '#',
-      $.identifier
-    ),
+    conditional_expression: ($) =>
+      prec.right("conditional", seq($._expression, "?", $._type_expression)),
 
-    _number: $ => /\d+/,
+    _type_expression: ($) =>
+      choice(
+        prec("application", $.type_application),
+        $.cell_ref_expression,
+        $.builtin_expression,
+        $.type_math_expression,
+        prec("unary", $.negate_expression),
+        $.parenthesized_type_expression,
+        prec("primary", $._ref_expression),
+        "#",
+      ),
 
-    _hex_number: $ => /[a-fA-F0-9_]+/,
+    _type_expression_no_application: ($) =>
+      choice(
+        $.builtin_expression,
+        $.type_math_expression,
+        prec("unary", $.negate_expression),
+        $.parenthesized_type_expression,
+        prec("primary", $._ref_expression),
+        "#",
+      ),
 
-    _binary_number: $ => /[01_]+/
-  }
+    _type_app_argument: ($) =>
+      choice(
+        $._simple_type_expr,
+        prec.right(1, $.cell_ref_expression),
+        prec.right(1, $.negate_expression),
+        prec.right(1, $.builtin_expression),
+        $._number,
+      ),
+
+    _simple_type_expr: ($) =>
+      choice($.identifier, "Any", $._number, $.parenthesized_type_expression),
+
+    math_expression: ($) =>
+      prec.left(
+        "math",
+        choice(
+          seq($._expression, "+", $._expression),
+          seq($._expression, "-", $._expression),
+          seq($._expression, "*", $._expression),
+          seq($._expression, "/", $._expression),
+        ),
+      ),
+
+    type_math_expression: ($) =>
+      prec.left(
+        "type_math",
+        choice(
+          seq($._type_expression, "+", $._type_expression),
+          seq($._type_expression, "-", $._type_expression),
+          seq($._type_expression, "*", $._type_expression),
+          seq($._type_expression, "/", $._type_expression),
+        ),
+      ),
+
+    comparison_expression: ($) =>
+      prec.left(
+        "comparison",
+        seq(
+          $._expression,
+          choice("<=", ">=", "!=", "=", "<", ">"),
+          $._expression,
+        ),
+      ),
+
+    bit_selection_expression: ($) => seq($._expression, ".", $._number),
+
+    negate_expression: ($) =>
+      prec.right(
+        "unary",
+        seq(
+          "~",
+          choice(
+            $._ref_expression,
+            $.parenthesized_expression,
+            $.parenthesized_type_expression,
+          ),
+        ),
+      ),
+
+    cell_ref_expression: ($) =>
+      prec.right(
+        "cell_ref",
+        seq(
+          "^",
+          choice(
+            $.identifier,
+            $.field_anonymous_def,
+            $.parenthesized_type_application,
+          ),
+        ),
+      ),
+
+    type_application: ($) =>
+      prec.right(
+        "application",
+        seq($.identifier, repeat1($._type_app_argument)),
+      ),
+
+    parenthesized_type_application: ($) =>
+      seq("(", $.identifier, repeat1($._type_expression), ")"),
+
+    builtin_expression: ($) =>
+      seq(
+        "(",
+        choice(
+          seq($.builtin_operator, choice($._expression, $._number)),
+          $.builtins_zero_args,
+        ),
+        ")",
+      ),
+
+    builtin_operator: ($) => choice("##", "#<", "#<=", "lenBits"),
+
+    builtins_zero_args: ($) => "#",
+
+    _ref_expression: ($) => prec("atom", choice($.identifier, $._number)),
+
+    parenthesized_expression: ($) => seq("(", $._expression, ")"),
+
+    parenthesized_type_expression: ($) => seq("(", $._type_expression, ")"),
+
+    comment: ($) =>
+      token(
+        choice(seq("//", /.*/), seq("/*", /[^*]*\*+([^/*][^*]*\*+)*/, "/")),
+      ),
+
+    identifier: ($) => /[a-zA-Z_][a-zA-Z0-9_]*/,
+
+    type_identifier: ($) => choice("Type", "#", $.identifier),
+
+    _number: ($) => /\d+/,
+
+    _hex_number: ($) => /[a-fA-F0-9_]+/,
+
+    _binary_number: ($) => /[01_]+/,
+  },
 });
